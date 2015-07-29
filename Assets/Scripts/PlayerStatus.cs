dbbdfeae4d854207659461b758967ccfb1dfcef1
wes.ehrlichman <wes.ehrlichman@asynchrony.com>
2015-07-29 12:54:24 -0500

    Writing a few integration tests to get the feel of things

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatus : MonoBehaviour {
+
+	public float hitPoints = 100;
+
+	public bool poisioned = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (poisioned) {
+			hitPoints -= Time.deltaTime * 10.0f;
+		}
+	}
+}

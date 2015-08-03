b0e446f6247d7fc71769efa4b441ade6e6e02ed7
wes.ehrlichman <wes.ehrlichman@asynchrony.com>
2015-08-03 00:11:10 -0500

    Completed Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,11 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float movementSpeed = 2.0f;
 	public InputHelper inputHelper;
+	Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+		animator = this.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -17,9 +18,19 @@ public class PlayerMovement : MonoBehaviour {
 		var axis = inputHelper.axisRaw();
 		if (axis != Vector2.zero) {
 			if (axis.magnitude >= 1) {
-				axis.Normalize();
+				axis.Normalize ();
+			}
+			if (Mathf.Abs (axis.x) > Mathf.Abs (axis.y)) {
+				animator.SetFloat ("HorizontalMovement", axis.x);
+				animator.SetFloat ("VerticalMovement", 0.0f);
+			} else {
+				animator.SetFloat ("HorizontalMovement", 0.0f);
+				animator.SetFloat ("VerticalMovement", axis.y);
 			}
 			transform.position += new Vector3 (axis.x * movementSpeed * inputHelper.deltaTime, axis.y * movementSpeed * inputHelper.deltaTime, 0);
+		} else {
+			animator.SetFloat ("HorizontalMovement", 0.0f);
+			animator.SetFloat ("VerticalMovement", 0.0f);
 		}
 	}
 }

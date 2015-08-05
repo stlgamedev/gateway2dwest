731ffcd7da2a30175242072f2abea8bcad77cc99
Randal James Phillips <caliber_mengsk@yahoo.com>
2015-08-05 02:07:14 -0500

    Work on Player Movement and Extending the Camera Object
    Added extra comments to player movement for those that don't know much
    about unity or programming.
    Added rigidbody2d component to player object in test scene
    Changed movement from transform.position + movement data to
    rigidbody.velocity. transform.position ignores collisions and will let
    you move through colliders.
    Changed axis to be implicitly Vector2.
    Removed axis check not equal to zero check, as this stops the velocity
    from being set when there is no input. (player couldn't stop walking
    unless there is a collision
    Moved idle animation parameter to work right with newer movement method.
    Added Camera Extension for Bounds and Extents. This should help with
    limiting the camera to a certain area later down the line.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,28 +7,30 @@ public class PlayerMovement : MonoBehaviour {
 	public float movementSpeed = 2.0f;
 	public InputHelper inputHelper;
 	Animator animator;
+    Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		animator = this.GetComponent<Animator> ();
+		animator = GetComponent<Animator> (); //In unity, GetComponent is implicit to the object the script is attached, thus "this" is not needed
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var axis = inputHelper.axisRaw();
-		if (axis != Vector2.zero) {
-			if (axis.magnitude >= 1) {
-				axis.Normalize ();
-			}
-			if (Mathf.Abs (axis.x) > Mathf.Abs (axis.y)) {
-				updateAnimationParameters(axis.x, 0.0f);
-			} else {
-				updateAnimationParameters(0.0f, axis.y);
-			}
-			transform.position += new Vector3 (axis.x * movementSpeed * inputHelper.deltaTime, axis.y * movementSpeed * inputHelper.deltaTime, 0);
-		} else {
-			updateAnimationParameters(0, 0);
+		Vector2 axis = inputHelper.axisRaw(); //Get's input from helper class. This allows for unit testing as well as changing input while inside of the game.
+		if ( Mathf.Abs(axis.magnitude) >= 1) { //Checks that values are not greater then one for input. Checks absolute value to fix errors in positive and negative directions.
+			axis.Normalize ();
 		}
+		if (Mathf.Abs (axis.x) > Mathf.Abs (axis.y)) {
+			updateAnimationParameters(axis.x, 0.0f);//Sets up walking left/right animation
+		} else if(Mathf.Abs (axis.x) < Mathf.Abs (axis.y)) {
+			updateAnimationParameters(0.0f, axis.y);//Sets up walking up/down animation
+		} else if(axis == Vector2.zero) {//Checking if it's equal to zero will allow animations when sliding against walls to still work
+            updateAnimationParameters(0, 0);//sets up idle animation
+		}
+		//transform.position += new Vector3 (axis.x * movementSpeed * inputHelper.deltaTime, axis.y * movementSpeed * inputHelper.deltaTime, 0);
+        //Setting a transform.position instead of using a system like a rigidbody ignores colliders.
+        rb.velocity = axis * movementSpeed; //Unity automatically applies a delta time to rigidbody velocity.
 	}
 
 	void updateAnimationParameters (float x, float y)

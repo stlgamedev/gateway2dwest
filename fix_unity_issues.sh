87aab5305813be396bba83af816ad538851381e1
wes.ehrlichman <wes.ehrlichman@asynchrony.com>
2015-08-14 12:51:38 -0500

    Improve unity fix-it script to do more improvin'

--- fix_unity_issues.sh
+++ fix_unity_issues.sh
@@ -5,6 +5,12 @@ guisystem_location=/Applications/Unity/Unity.app/Contents/UnityExtensions/Unity
 echo $path
 
 mkdir -p ~/tmp/
-mv  $guisystem_location/GUISystem ~/tmp
+mv $guisystem_location/GUISystem ~/tmp
+mv $guisystem_location/Networking ~/tmp
+mv $guisystem_location/UnityAnalytics ~/tmp
+
 $unity_location -projectPath $path -quit
 mv ~/tmp/GUISystem $guisystem_location
+mv ~/tmp/Networking $guisystem_location
+mv ~/tmp/UnityAnalytics $guisystem_location
+

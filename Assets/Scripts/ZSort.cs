51856c4b072b02a9e83d7254ab732565a43ebdcc
Randal James Phillips <caliber_mengsk@yahoo.com>
2015-08-30 21:28:36 -0500

    Added sphere for placeholder enemy - No ai yet Added sphere explode/death animation Added shadow texture for use later with other enemies Added Zsort script to replace sorting of z-order of objects in the same sort order index in player movement. New file does this instead so that enemies and other objects can also have the zsort script on it later down the line.

---
+++ Assets/Scripts/ZSort.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZSort : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y / 1000);
+	}
+}

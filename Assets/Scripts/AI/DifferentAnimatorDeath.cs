e2c2529595b2e04af5fcfc660af6ba731d91a6d1
Randal James Phillips <caliber_mengsk@yahoo.com>
2015-11-14 16:49:14 -0600

    Forgot to attach the attack sound to the player sprite object. Also forgot to add in untracked files (images, sound effects, a few scripts and a lot of files from updating the unittest from the asset store)

---
+++ Assets/Scripts/AI/DifferentAnimatorDeath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DifferentAnimatorDeath : MonoBehaviour {
+    public Animator remoteAnimator;
+    public Animation animationToPlay;
+    public void Die()
+    {
+        if(remoteAnimator != null && animationToPlay != null)
+        {
+            remoteAnimator.Play(animationToPlay.name);
+        }
+    }
+}
---
+++ Assets/Scripts/DestroyOnDeath.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnDeath : MonoBehaviour {
+    public GameObject objectToDestroyOnDeath;
+    public GameObject[] objectsToDisable;
+    public void DestroyObject()
+    {
+        Debug.Log("Destroying " + objectToDestroyOnDeath.name);
+        Destroy(objectToDestroyOnDeath);
+    }
+    public void Die()
+    {
+        foreach(GameObject go in objectsToDisable)
+        {
+            foreach (Renderer r in go.GetComponents<Renderer>())
+            {
+                r.enabled = false;
+            }
+            foreach (Collider c in go.GetComponents<Collider>())
+            {
+                c.enabled = false;
+            }
+            foreach (Collider2D c in go.GetComponents<Collider2D>())
+            {
+                c.enabled = false;
+            }
+        }
+        GetComponent<Animator>().Play("Die");
+    }
+}

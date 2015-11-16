7fe07bfdecd23c673cb01e9a08fa2bb5d7742184
Randal James Phillips <caliber_mengsk@yahoo.com>
2015-11-15 21:43:12 -0600

    Added tracking to character select scene and script, though nothing is really noteable about them yet.

---
+++ Assets/Scripts/Menus/CharacterSelect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelect : MonoBehaviour {
+    [Range(1,4)]
+    public int playerId = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
